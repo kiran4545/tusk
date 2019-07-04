661d4ee867772232685fa64952f5579a8b89b98e
kiran4545 <phskramaraju@gmail.com>
2019-07-04 11:17:37 +0530

    Water needed

--- hungry.py
+++ hungry.py
@@ -2,5 +2,10 @@ hungry = input("are u hungry?")
 if hungry=="yes":
 print("eat samosa")
 print("eat pizza")
+print("eat burger")
+else:
+thirsty=input("are you thirsty")
+if(thirsty)
+print("drink water")
 else
 print("do your homework")
